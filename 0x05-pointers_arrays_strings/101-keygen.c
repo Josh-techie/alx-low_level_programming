009d8a2706b18e765cea8acd9a226b6f11a08860
Joe <youse2002@outlook.fr>
2023-03-27 18:14:20 -0700

    pass crack

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,50 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LEN 12
-
 /**
- * main - Generates random valid passwords for the program 101-crackme
+ * main - generates a random valid password for 101-crackme program
  *
- * Done by Joe
  * Return: Always 0
  */
 int main(void)
 {
-int i, sum, diff;
-char password[PASSWORD_LEN + 1];
-time_t t;
-srand((unsigned int) time(&t));
-/* Generate random characters until the sum of their ASCII codes is 2772 */
+int i, sum, diff, c;
+char password[100];
+
+srand(time(NULL));
 sum = 0;
-for (i = 0; i < PASSWORD_LEN; i++)
+i = 0;
+while (sum < 2772 - 122)
 {
-password[i] = rand() % 94 + 33;
-sum += password[i];
-}
-/* Calculate the difference between the sum and the target ASCII value */
-diff = 2772 - sum;
-/* Add the difference to the last character of the password */
-password[PASSWORD_LEN - 1] += diff
-/* Check if any character is out of range or a null byte */
-for (i = 0; i < PASSWORD_LEN; i++)
-{
-if (password[i] < 33 || password[i] > 126 || password[i] == 0)
-{
-diff = (password[i] < 33) ? 33 - password[i] : 126 - password[i];
-password[i] += diff;
-}
-}
-password[PASSWORD_LEN] = '\0';
-printf("%s", password);
-if (strcmp(password, "Tada! Congrats") == 0)
-{
-printf("\n");
-return (0);
-}
+c = rand() % 62;
+if (c < 10)
+password[i++] = '0' + c;
+else if (c < 36)
+password[i++] = 'a' + c - 10;
 else
-{
-fprintf(stderr, "\nWrong password\n");
-return (1);
+password[i++] = 'A' + c - 36;
+sum += password[i - 1];
 }
+diff = sum - (2772 - 122);
+password[i] = diff;
+printf("%s", password);
+return (0);
 }
