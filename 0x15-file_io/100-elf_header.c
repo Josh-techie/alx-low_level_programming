7e88265370022f789a1a8f807f1a6d6fcfd4315d
Joe <youse2002@outlook.fr>
2023-05-08 18:58:28 -0700

    task done

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -111,7 +111,7 @@ printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 *  * print_version - Prints the version of an ELF header.
 *   * @e_ident: A pointer to an array containing the ELF version.
 */
-voidprint_version(unsigned char *e_ident)
+void print_version(unsigned char *e_ident)
 {
 printf(" Version: %d",
 e_ident[EI_VERSION]);
